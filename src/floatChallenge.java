1f15a610575db37433c45c67d2b96f1ec95ca917
wanjohi lewis <wanjohilewi7@gmail.com>
2024-05-19 21:11:15 +0300

    how to use float data type

---
+++ src/floatChallenge.java
@@ -0,0 +1,7 @@
+public class floatChallenge {
+    public static void main(String[] args){
+        double myPounds = 10d;
+        double convertedKilograms = myPounds * 0.45359237d;
+        System.out.println("Converted kilograms= " + convertedKilograms);
+    }
+}
---
+++ src/floatingPrecision.java
@@ -0,0 +1,20 @@
+public class floatingPrecision {
+    public static void main(String[] args){
+        float myMaxFloatValue = Float.MAX_VALUE;
+        float myMinFloatValue = Float.MIN_VALUE;
+        System.out.println("Float maximum value = " + myMinFloatValue);
+        System.out.println("Float minimum value = " + myMinFloatValue);
+
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        double myMinDoubleValue = Double.MIN_VALUE;
+        System.out.println("Double maximum value = " + myMinDoubleValue);
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+
+        int myIntValue = 5 /2;
+        float myFloatValue = 5f/2;
+        double myDoubleValue = 5d/2;
+        System.out.println("MyIntValue= " + myIntValue);
+        System.out.println("myFloatValue= " + myFloatValue);
+        System.out.println("myDoubleValue= " + myDoubleValue);
+    }
+}
